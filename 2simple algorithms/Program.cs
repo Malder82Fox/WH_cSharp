0cc42d2034ab9c993887b92f049eb48f987239cc
Malder82PChome <pto.nz@yandex.ru>
2023-12-06 01:45:50 +0600

    done task004 and test - ok

--- 2simple algorithms/Program.cs
+++ 2simple algorithms/Program.cs
@@ -1,37 +1,18 @@
-﻿//Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
-//и показывает наибольшую цифру числа.
-//40 ->  4; 96 -> 9; 72 -> 7
+﻿//Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+//568 ->  5,6,8; 9 -> 9; 7242 -> 7,2,4,2
 
 Console.Clear();
 
-Console.Write("Enter your number [10-99] - ");
+Console.Write("Enter your number - ");
 string strNum = Console.ReadLine();
 int.TryParse(strNum, out int Num);
 
-if (Num < 10 || Num > 99)
+int count = strNum.Length;
+int[] Array = new int[count];
+
+for (int i = count - 1; i >= 0; i--)
 {
-    Console.WriteLine("It is not correct !!! `10-99`");
+    Array[i] = Num % 10;
+    Num /= 10;
 }
-
-else
-{
-    int[] Array = new int[2];
-    for (int i = 1; i >= 0; i--)
-    {
-        Array[i] = Num % 10;
-        Num /= 10; // can use "Num = Num / 10"
-    }
-
-    int FirstNum = Array[0];
-    int SecondNum = Array[1];
-
-    if (FirstNum >= SecondNum)
-    {
-        Console.WriteLine($"Larger number `{FirstNum}` in digit `{strNum}`");
-    }
-    else
-    {
-        Console.WriteLine($"Larger number `{SecondNum}` in digit `{strNum}`");
-    }
-}
-
+Console.WriteLine(string.Join(", ", Array));   
